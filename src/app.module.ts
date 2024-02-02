cea7eae09cd22f5e33cfc691d1ce3ef044284f9d
Erfan Yousefo <erfanyousefi.co@gmail.com>
2024-02-02 17:19:03 +0330

    create new project and add typeorm config and connect to database

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm"
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "root",
+      database: "typeorm",
+      autoLoadEntities: true,
+      entities: [],
+      synchronize: true
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
